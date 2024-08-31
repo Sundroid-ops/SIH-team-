af75ab2a17ac32f9793eb08459f6442602c0a5b6
YojirO112 <akashdas13400@gmail.com>
2024-09-01 00:00:01 +0530

    product and collection rote

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,14 @@ const mongoConnect = require("./config/dbConfig");
 require("dotenv").config();
 
 //Import Routes
-const Hello = require("./routes/hello")
+const Hello = require("./routes/hello");
+const productRouter  = require('./routes/ProductRoute');
+const  collectionRouter  = require('./routes/CollectionsRoute');
 
 //Routes
 app.use("/hello", Hello);
+app.use("/api/product", productRouter)
+app.use("/api/collection", collectionRouter)
 
 mongoConnect();
 const port = 4000
