3fd967e914ae8f0738cd06e6dc3002365354975f
YojirO112 <akashdas13400@gmail.com>
2024-09-01 05:02:06 +0530

    check .env file and update cloudinary part

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoConnect = require("./config/dbConfig");
+const connectCloudinary = require("./config/cloudinary")
 require("dotenv").config();
 
 //Import Routes
@@ -8,14 +9,16 @@ const Hello = require("./routes/hello");
 const productRouter  = require('./routes/ProductRoute');
 const  collectionRouter  = require('./routes/CollectionsRoute');
 
+mongoConnect();
+const port = 4000
+connectCloudinary();
+
+app.use(express.json())
+
 //Routes
 app.use("/hello", Hello);
 app.use("/api/product", productRouter)
 app.use("/api/collection", collectionRouter)
-
-mongoConnect();
-const port = 4000
-
 
 app.listen(port,()=>{
     console.log(`App is running on Port Number ${port}`);
